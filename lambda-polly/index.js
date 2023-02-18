90159f33d676a655e676b796d4d40f5dd52eed4b
ksdyb.park <ksdyb@amazon.com>
2023-02-19 00:01:21 +0900

    update

--- lambda-polly/index.js
+++ lambda-polly/index.js
@@ -23,7 +23,7 @@ exports.handler = async (event) => {
 
     const id = body.Id;
     const bucket = body.Bucket;
-    const name = body.key;    
+    const name = body.Key;    
     const text = body.Text;
     console.log('text: '+text);
 
